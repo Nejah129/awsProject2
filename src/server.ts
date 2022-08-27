cdcea6b475dd8e4d5f4153f40a9afeca18371eec
Nejah129 <nejahyengui999@gmail.com>
2022-08-27 23:12:29 +0100

    submit final

--- src/server.ts
+++ src/server.ts
@@ -54,9 +54,7 @@ import { filterImageFromURL, deleteLocalFiles } from "./util/util";
     }
   });
 
-  app.get("/", async (req, res) => {
-    res.send("try GET /filteredimage?image_url={{}}");
-  });
+
 
   // Start the Server
   app.listen(port, () => {
